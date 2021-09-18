9f2c3bcc63850cd35f6e90e122b489a9f97bf21a
Katekukis <90500075+Katekukis@users.noreply.github.com>
2021-09-18 11:07:46 +0300

    Initial commit

---
+++ src/homework/Primer_for.java
@@ -0,0 +1,11 @@
+package homework;
+import java.util.Scanner;
+
+public class Primer_for {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Привет");
+        }
+    }
+}
